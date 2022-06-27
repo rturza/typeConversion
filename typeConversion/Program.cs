a8622537a4fbe4d73e38167c485fb341ac9bf490
TurXo <TurXo@DESKTOP-8B4G5P7>
2022-06-27 17:52:08 +0600

    Add project files.

---
+++ typeConversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace typeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*byte b = 1;
+            int i = b;
+            Console.WriteLine(i);*/
+
+            /*var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);*/
+
+            try
+            {
+                var number = "1234";
+                byte b = Convert.byte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("This number couldn't be converted to byte.");
+            }
+        }
+    }
+}
